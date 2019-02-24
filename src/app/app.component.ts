e058362beaa17fed5051f8858c0b3f1401a4e664
unknown <marouan.alioui@gmail.com>
2019-02-24 17:47:15 +0100

    modified:   .angular-cli.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/app.module.ts 	new file:   src/app/post-list-item/post-list-item.component.css 	new file:   src/app/post-list-item/post-list-item.component.html 	new file:   src/app/post-list-item/post-list-item.component.spec.ts 	new file:   src/app/post-list-item/post-list-item.component.ts 	new file:   src/app/post-list/post-list.component.css 	new file:   src/app/post-list/post-list.component.html 	new file:   src/app/post-list/post-list.component.spec.ts 	new file:   src/app/post-list/post-list.component.ts 	modified:   src/index.html 	new file:   srcexo.zip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+
+export class AppComponent implements OnInit {
+  posts = [
+    {
+      title: 'mon post1',
+      content: 'du blabllblblbblablabl',
+      loveIts: 1,
+      created_at: new Date()
+    },
+    {
+      title: 'mon post12',
+      content: 'du blabllblblbblablabl',
+      loveIts: 0,
+      created_at: new Date()
+    },
+    {
+      title: 'mon post13',
+      content: 'du blabllblblbblablabl',
+      loveIts: -2,
+      created_at: new Date()
+    }];
+  constuctor() {
+
+  }
+
+  ngOnInit() {
+
+  }
+
 }
